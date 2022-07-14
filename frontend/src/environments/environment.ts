ab78b199671dc9e3bebb7fee03f4830161be5f69
Hassan Yahya A <hassanyahya400@gmail.com>
2022-07-14 10:41:08 +0100

    add credentials

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,15 @@
 /* @TODO replace with your variables
  * ensure all variables on this page match your project
  */
+// let secret = require('secret')
 
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'dev-t1ko47b4.us', // the auth0 domain prefix
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: 'dq58Z05DCGsuqdLAYxoTS19azOVINTtc', // the client id generated for the auth0 app
+    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
---
+++ frontend/src/environments/secret.js
@@ -0,0 +1,4 @@
+export const URL = ''
+export const AUDIENCE = ''
+export const CLIENT_ID = ''
+export const CALL_BACK_URL = ''
